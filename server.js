8d7983b07bec2438441979cb6538eeee3d830a7e
Janelle <janellephalon@gmail.com>
2022-04-07 14:14:00 -0400

    working server and updated files

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
 
 // Instantiate the Server
 const app = express();
@@ -8,20 +7,17 @@ const app = express();
 // Initate Port
 const PORT = process.env.PORT || 3001;
 
-// Initialize Notes
-let userNotes = [];
-
-// Parsing
+// Data Parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/assets", express.static("./assets"));
 
 // Routes
-const apiRoutes = require('./routes/apiRoutes.js');
-const htmlRoutes = require('./routes/htmlRoutes.js');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // Listener 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`Now listening on port ${PORT}!`);
 });
 
